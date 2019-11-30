5395389a373437b7718b8362a6e8f8cbebfbda44
Alexandre Lopes Pedone <alpedone@gmail.com>
2019-11-30 10:58:31 -0300

    Update firefoxperpetuo.sh

--- firefoxperpetuo.sh
+++ firefoxperpetuo.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# Se o Firefox for fechado, o processo do navegador executa novamente.
-#Colocar o script no /usr/bin/firefoxperpetuo.sh e dar permissão de execução no arquivo:
-#chmod +x /usr/bin/firefoxperpetuo.sh
-
 xsetroot -solid white &
 # firfox em modo perpétuo
 while true ; do
